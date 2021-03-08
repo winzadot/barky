859b931d31704d7bcbe3b5ae6146be8dd8a13d64
Nawin Mathi <mnawin@gmail.com>
2021-03-07 21:40:30 -0600

    Test Github Starred

---
+++ Barky/test_database.py
@@ -0,0 +1,115 @@
+# the database module is much more testable as its actions are largely atomic
+# that said, the database module could certain be refactored to achieve decoupling
+# in fact, either the implementation of the Unit of Work or just changing to sqlalchemy would be good.
+
+import os
+from datetime import datetime
+import sqlite3
+import requests
+import pytest
+import githubaccount
+
+from database import DatabaseManager
+
+@pytest.fixture
+def database_manager() -> DatabaseManager:
+    """
+    What is a fixture? https://docs.pytest.org/en/stable/fixture.html#what-fixtures-are
+    """
+    filename = "test_bookmarks.db"
+    dbm = DatabaseManager(filename)
+    # what is yield? https://www.guru99.com/python-yield-return-generator.html
+    yield dbm
+    dbm.__del__()           # explicitly release the database manager
+    os.remove(filename)
+
+
+def test_database_manager_create_table(database_manager):
+    # arrange and act
+    database_manager.create_table(
+        "bookmarks",
+        {
+            "id": "integer primary key autoincrement",
+            "title": "text not null",
+            "url": "text not null",
+            "notes": "text",
+            "date_added": "text not null",
+        },
+    )
+
+    #assert
+    conn = database_manager.connection
+    cursor = conn.cursor()
+
+    cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='bookmarks' ''')
+
+    assert cursor.fetchone()[0] == 1
+
+    #cleanup
+    # this is probably not really needed
+    database_manager.drop_table("bookmarks")
+
+
+def test_database_manager_add_bookmark(database_manager):
+
+    # arrange
+    database_manager.create_table(
+        "bookmarks",
+        {
+            "id": "integer primary key autoincrement",
+            "title": "text not null",
+            "url": "text not null",
+            "notes": "text",
+            "date_added": "text not null",
+        },
+    )
+
+    data = {
+        "title": "test_title",
+        "url": "http://example.com",
+        "notes": "test notes",
+        "date_added": datetime.utcnow().isoformat()        
+    }
+
+    # act
+    database_manager.add("bookmarks", data)
+
+    # assert
+    conn = database_manager.connection
+    cursor = conn.cursor()
+    cursor.execute(''' SELECT * FROM bookmarks WHERE title='test_title' ''')    
+    assert cursor.fetchone()[0] == 1    
+
+def test_get_github_starred_bookmark():
+  '''  githubtest_username = "winzadot" '''
+  ''' Check this repo is there repo["name"]=='Assignment1-RomanNumbers' '''
+
+next_page_of_results = f"https://api.github.com/users/winzadot/starred"
+
+while next_page_of_results:
+            stars_response = requests.get(
+                next_page_of_results,
+                headers={"Accept": "application/vnd.github.v3.star+json"},
+            )
+            next_page_of_results = stars_response.links.get("next", {}).get("url")
+            for repo_info in stars_response.json():
+                repo = repo_info["repo"]
+            assert repo["name"]=='Assignment1-RomanNumbers'
+
+
+def test_get_github_starred_time():
+  '''  githubtest_username = "winzadot" '''
+  ''' Check this repo is there repo["name"]=='Assignment1-RomanNumbers' '''
+'''starred_at '2021-03-08T00:06:34Z'''
+
+next_page_of_results = f"https://api.github.com/users/winzadot/starred"
+while next_page_of_results:
+            stars_response = requests.get(
+                next_page_of_results,
+                headers={"Accept": "application/vnd.github.v3.star+json"},
+            )
+            next_page_of_results = stars_response.links.get("next", {}).get("url")
+            for repo_info in stars_response.json():
+                repo = repo_info["repo"]
+            assert repo_info["starred_at"]!=None
+            assert  repo_info["starred_at"]=='2021-03-08T00:06:34Z'
